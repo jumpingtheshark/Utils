f195200f5cb68b00adbe447da43e61f1ac4c20d0
Michael Rubinstein <mrubinstein@vitu.com>
2019-03-19 17:12:07 -0500

    hello

---
+++ Utils/DBHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Utils
+{
+    public class DBHelper
+    {
+        public string _constring = "";
+        public SqlConnection _connection;
+
+
+        public DataTable getTable(string query)
+        {
+
+           SqlDataAdapter a = new SqlDataAdapter(query, _connection);
+            DataTable dt = new DataTable();
+            a.Fill(dt);
+            return dt;
+        }
+
+
+        public Dictionary<string, string> dt2Diq(DataTable dt)
+        {
+
+            Dictionary<string, string> d = new Dictionary<string, string>();
+            foreach (DataRow dr in dt.Rows)
+                d.Add(dr[0].ToString(), dr[1].ToString());
+
+            return d;
+
+        }
+
+        public List<string> getList(string query)
+        {
+            DataTable dt = getTable(query);
+
+            List<string> l = new List<string>();
+
+            foreach (DataRow dr in dt.Rows)
+                    l.Add(dr[0].ToString());
+
+            return l;
+        }
+
+
+        public List<string> dt2List(DataTable dt)
+        {
+            List<string> l = new List<string>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                l.Add(dr[0].ToString());
+
+            }
+
+            return l;
+
+        }
+
+
+        public void openConnection()
+        {
+            _connection.ConnectionString = _constring;
+            _connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            _connection.Close();
+        }
+
+
+
+        // connection must be opened and closed manually. 
+        public void runCommand(string command)
+        {
+
+
+
+            var cmd = new SqlCommand(command);
+            cmd.Connection = _connection;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("exception: ");
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+
+
+    }
+
+}
---
+++ Utils/Utils.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.IO;
+using System.Data;
+using System.Collections.Generic;
+
+
+namespace Utils
+{
+    class Utils
+    {
+
+
+        public string readAllText(string path)
+        {
+            string s= File.ReadAllText(path);
+            return s;
+        }
+
+        public string[] readAllLines(string path)
+        {
+
+            string[] lines = File.ReadAllLines(path);
+            return lines;
+        }
+
+
+        //todo
+        public DataTable csv2DT(string path)
+        {
+            return new DataTable();
+
+        }
+       
+        //todo 
+
+        public List<string[]> csv2List1(string path)
+        {
+            return new List<string[]>();
+
+        }
+
+
+        public List<List<string>> csv2List2 (string path)
+        {
+            return new List<List<string>>();
+
+        }
+
+
+    }
+}
