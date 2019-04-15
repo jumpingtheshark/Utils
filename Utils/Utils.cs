13b99c7742e6c4ea3df18760d965ff20f9a000a9
Michael Rubinstein <mrubinstein@vitu.com>
2019-04-15 14:08:13 -0500

    date f()'s

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -61,17 +61,32 @@ namespace Utils
         }
 
 
-        // todo 
+        // takes mm/dd/yyyy and gives back yyyymmdd
         public  string datestring2string (string s)
         {
 
-            return "";
+            string[] sarray = s.Split('/');
+            string month, date, year;
+            month = sarray[0].PadLeft(2, '0');
+            date = sarray[1].PadLeft(2, '0');
+            year = sarray[2].PadLeft(4, '0');
+
+            string sdate = year + month + date;
+            return sdate;
+            
         }
 
         public DateTime datestring2DateTime (string s)
         {
+            string[] sarray = s.Split('/');
+           int month, date, year;
+            month =   Convert.ToInt16(sarray[0]);
+            date = Convert.ToInt16(sarray[1]);
+            year = Convert.ToInt16(sarray[2]);
 
-            return new DateTime();
+            DateTime dt = new DateTime(year, month, date);
+            return dt;
+         
 
         }
     }
