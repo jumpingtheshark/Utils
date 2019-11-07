8f43c21fe18eacac8c0b3ac1da9787de6c370942
Michael R <mike.rubinstein@gmail.com>
2019-11-07 15:43:51 -0600

    json stuff

---
+++ Utils/Json.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Utils
+{
+	class Json
+	{
+
+
+		public static Dictionary<string, string> dnr(string json)
+		{
+		
+			json = Filer.readAllText(@"C:\a1\Utils\Utils\j1.json");
+	  		Dictionary<string, string> rv = JsonConvert.DeserializeObject<Dictionary<string, string>>(json, new JsonSerializerSettings(). );
+			return rv;
+		}
+
+	}
+}
--- Utils/Program.cs
+++ Utils/Program.cs
@@ -20,34 +20,8 @@ namespace Utils
 
 
 		        {
-			string charge = "DEBIT,3 / 11 / 2019,\"NUTELLA CAFE, CHI 001 CHICAGO IL             03/10\",-46.4,DEBIT_CARD,440.05,";
 
-			string charge2=Stringer.removeCharFromInterval(charge, ',', '^');
-
-
-
-			CSVHelper.csv2ListStringArray(@"c:\a2\f.csv");
-
-   /*
-   //char c1 = ',';
-			char c2 = '^';
-
-			string charge = "DEBIT,3 / 11 / 2019,\"NUTELLA CAFE, CHI 001 CHICAGO IL             03/10\",-46.4,DEBIT_CARD,440.05,";
-
-			int p1 = charge.IndexOf("\"");
-			int p2 = charge.IndexOf("\"", p1+1);
-			var charge2 = charge.ToCharArray();
-			for (int i =0; i<charge2.Length; i++)
-			{
-
-				if (i > p1 && i < p2 && charge[i] == c1)
-					charge2[i] = c2;
-					
-			}
-			charge = new string(charge2);
-
-
-	*/		
+			 Dictionary<string, string> dnr= Json.dnr("");
             
            
 
